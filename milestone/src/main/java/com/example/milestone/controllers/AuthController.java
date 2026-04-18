33a3fc09fe48d89c50ca48607c79ea18d46ad233
Rodrigo Cornidez <cornidez04@gmail.com>
2026-04-18 11:24:00 -0700

    add thymeleaf, bootstrap, a base template, root, and sample login & register pages. Also created home controller to serve root, and auth controller to serve login & register

---
+++ milestone/src/main/java/com/example/milestone/controllers/AuthController.java
@@ -0,0 +1,18 @@
+package com.example.milestone.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "pages/login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "pages/register";
+    }
+}
